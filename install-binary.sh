35d3b3cf11fa89c3f8a469c569e007a98a083bcf
Youta Ogino <ogino_yota@cyberagent.co.jp>
2020-11-12 11:39:17 +0900

    workaround

--- install-binary.sh
+++ install-binary.sh
@@ -8,7 +8,7 @@ export GREP_COLOR="never"
 
 HELM_MAJOR_VERSION=$(helm version --client --short | awk -F '.' '{print $1}')
 
-: ${HELM_PLUGIN_DIR:="$(helm home --debug=false)/plugins/helm-diff"}
+: ${HELM_PLUGIN_DIR:="${HELM_HOME}/plugins/helm-diff"}
 
 # Convert the HELM_PLUGIN_DIR to unix if cygpath is
 # available. This is the case when using MSYS2 or Cygwin
